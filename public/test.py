a7212e5489046da30336486c225a05d0e6027fa9
item21 <martinbilansky@gmail.com>
2021-11-25 17:23:46 +0100

    file writer

--- public/test.py
+++ public/test.py
@@ -1,15 +1,10 @@
-#!/usr/bin/python3 -u
-import time
-import sys
+import os
+import time 
+
 
 
 for i in range(1,10):
-    # sys.stderr.write(str(i))
-    time.sleep(1)
-    sys.stdout.write(str(i))
-    print(i)
-    # print("TEST\n")
-    # time.sleep(1)
-    # print >> sys.stdout, str(i)
-    # print(str(i)+'', file = sys.stdout)
-    # time.sleep(1)
+    f = open("demofile2.txt", "a")
+    f.write(str(i))
+    f.close()
+    time.sleep(2)
