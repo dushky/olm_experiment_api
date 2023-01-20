c29154459b2e881d660edc432b2d3475603786d2
tomaspopik <tpopik1999@gmail.com>
2023-01-20 13:36:12 +0100

    fix: create experiment

--- app/GraphQL/Mutations/RunScript.php
+++ app/GraphQL/Mutations/RunScript.php
@@ -28,7 +28,7 @@ class RunScript
         $software = $args['runScriptInput']['device']['software'];
         $scriptName = $args['runScriptInput']['scriptName'];
         $scriptFileName = Helpers::getScriptName($scriptName, base_path()."/server_scripts/$deviceName/$software");
-        
+
         if ($scriptFileName == null) {
             broadcast(new DataBroadcaster(null, $device->name, "No such script or file in directory", false));
             return;
@@ -40,10 +40,10 @@ class RunScript
         } else {
             $schemaFileName = explode(".", $args['runScriptInput']['fileName']);
         }
-        
+
         // Log::channel('server')->error("ERRORMESSAGE: " . Helpers::getSchemaNameForLocalStart($software));
         $args['runScriptInput']['inputParameter'] = $args['runScriptInput']['inputParameter'] . ",uploaded_file:". storage_path('tmp/uploads/') . ",file_name:". $schemaFileName[0];
-        
+
         Log::channel('server')->error("ERRORMESSAGE: " . $args['runScriptInput']['inputParameter']);
         $experiment = ExperimentLog::create([
             'device_id' => $device->id,
@@ -51,15 +51,15 @@ class RunScript
             'output_path' => $fileName,
             'software_name' => $software,
             'schema_name' => $schemaFileName[0],
-            'process_pid' => '',
+            'process_pid' => -1,
             'started_at' => date("Y-m-d H:i:s")
         ]);
 
         $readingProcess = new StartReadingProcess($date, $fileName, $path, $device, $args, $experiment, $deviceName);
         dispatch($readingProcess)->onQueue("Reading");
-        
+
         return [
-            'status' => 'success', 
+            'status' => 'success',
             'experimentID' => $experiment->id,
             'errorMessage' => ''
         ];
