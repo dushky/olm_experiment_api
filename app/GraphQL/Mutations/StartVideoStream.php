4faf1f47171536bf0d0773398cfbbfab5f9935ec
tomaspopik <tpopik1999@gmail.com>
2023-04-23 12:33:15 +0200

    feat: add config for exit video stream where there is no clients

--- app/GraphQL/Mutations/StartVideoStream.php
+++ app/GraphQL/Mutations/StartVideoStream.php
@@ -7,9 +7,6 @@ use Symfony\Component\Process\Process;
 
 class StartVideoStream
 {
-
-    const EXIT_ON_NO_CLIENTS_SEC = 120;
-
     /**
      * @param null $_
      * @param array<string, mixed> $args
@@ -22,7 +19,9 @@ class StartVideoStream
             return ["isRunning" => false, "status" => "Camera port is NOT set"];
         }
 
-        $process = Process::fromShellCommandline("~/ustreamer/ustreamer --device=$device->camera_port --host=0.0.0.0 --port=9001 --process-name-prefix='ustreamer-$device->name' --exit-on-no-clients=" . self::EXIT_ON_NO_CLIENTS_SEC . " --allow-origin=\*");
+        $EXIT_ON_NO_CLIENTS_SEC = config('video-stream.exit_on_no_clients_sec');
+
+        $process = Process::fromShellCommandline("~/ustreamer/ustreamer --device=$device->camera_port --host=0.0.0.0 --port=9001 --process-name-prefix='ustreamer-$device->name' --exit-on-no-clients=$EXIT_ON_NO_CLIENTS_SEC --allow-origin=\*");
 
         $process->start();
         sleep(1);
---
+++ config/video-stream.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Exit the video stream when there are no clients
+    |--------------------------------------------------------------------------
+    |
+    | The time in seconds that must elapse when no clients
+    | are connected to the video stream before the stream is shut down.
+    |
+    */
+
+    'exit_on_no_clients_sec' => 120,
+];
